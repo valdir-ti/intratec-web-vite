194b4c76e310c9f3d2840a61d6840cb7e94271db
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2023-01-23 11:50:47 -0300

    feat: config PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,39 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     minify: true,
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: "intratec-web-vite",
+        short_name: "intratec-web-vite",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        lang: "en",
+        scope: "/",
+        icons: [
+          {
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+        theme_color: "#ffffff",
+      },
+    }),
+  ],
 });
