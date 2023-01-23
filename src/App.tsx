edf66fce54887c669dc6cc722b2bd532f5e9a4a2
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2023-01-23 15:38:21 -0300

    feat: create empty components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { Dashboard } from "./components/Dashboard";
+import { Footer } from "./components/Footer";
+import { Navbar } from "./components/Navbar";
+import { Sidebar } from "./components/Sidebar";
+
+import "./App.scss";
+
 function App() {
-  return <div>Intratec Tecnologia Dashboard</div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <Sidebar />
+      <Dashboard />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
