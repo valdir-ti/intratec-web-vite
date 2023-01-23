f67458aa0e414bea5b1652bf6ecff15557efd27b
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2023-01-23 17:33:10 -0300

    feat: add toogle sidebar button

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,9 +1,17 @@
+import MenuIcon from "@mui/icons-material/Menu";
+
 import "./styles.scss";
 
 export const Navbar = () => {
+  function handleToggleSidebar() {
+    alert("Toggle sidebar");
+  }
+
   return (
     <nav className="navbar">
-      <div className="button">Button</div>
+      <div className="button">
+        <MenuIcon onClick={handleToggleSidebar} />
+      </div>
       <div className="menu">Menu</div>
     </nav>
   );
