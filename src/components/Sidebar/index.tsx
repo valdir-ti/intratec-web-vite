770465ce7379a3918f61cae12877644ff010bd28
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2023-01-24 18:27:53 -0300

    feat: add close button on mobile screen

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,4 +1,5 @@
 import { useContext } from "react";
+import { Close } from "@mui/icons-material";
 
 import { SidebarContext } from "../../context/sidebar/sidebarContext";
 
@@ -10,9 +11,16 @@ export const Sidebar = () => {
     dispatch: SidebarDispatch,
   } = useContext(SidebarContext);
 
+  function handleClose() {
+    SidebarDispatch({ type: "TOGGLE_SIDEBAR" });
+  }
+
   return (
     <div className={`sidebar ${open ? "sidebar-open" : "sidebar-close"}`}>
-      <div className="company-name">Intratec</div>
+      <div className="company-name">
+        {open ? "Intratec Tecnologia" : "Intratec"}
+        <Close onClick={handleClose} className="close-button" />
+      </div>
       <div className="menu">Menu</div>
     </div>
   );
