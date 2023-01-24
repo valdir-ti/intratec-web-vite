79180ef39e333336e0c1eab987fcc6a362339fd6
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2023-01-24 09:53:51 -0300

    feat: add cards

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,13 @@
+import "./styles.scss";
+
+type CardProps = {
+  color: string;
+};
+
+export const Card = (props: CardProps) => {
+  return (
+    <div className="card">
+      <p>Card {props.color}</p>
+    </div>
+  );
+};
--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,5 +1,13 @@
+import { Card } from "../Card";
 import "./styles.scss";
 
 export const Dashboard = () => {
-  return <div className="dashboard">Dashboard</div>;
+  return (
+    <div className="dashboard">
+      <Card color="green" />
+      <Card color="gray" />
+      <Card color="blue" />
+      <Card color="yellow" />
+    </div>
+  );
 };
